08b4d863f666bfc289965c1dcd8b4c211f8934f4
David Dempsey <David Dempsey>
2017-03-22 23:04:55 +0000

    Initial File Dump

---
+++ TembooAccount.h
@@ -0,0 +1,3 @@
+#define TEMBOO_ACCOUNT "davidmd97"  // your Temboo account name 
+#define TEMBOO_APP_KEY_NAME "myFirstApp"  // your Temboo app key name
+#define TEMBOO_APP_KEY  "xQgwKrqLMWmslUQy2kTsjMwJg29DqeyV"  // your Temboo app key
